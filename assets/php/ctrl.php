900f4b165e19e682b9650b29f4a93a2b7b60abc3
Robin.Habran <robinhabran@gmail.com>
2020-02-04 12:27:52 +0100

    ex7

--- assets/php/ctrl.php
+++ assets/php/ctrl.php
@@ -21,3 +21,51 @@ $requeteShowTypeSql = 'SELECT `type` FROM `showTypes` ORDER BY `type`';
 $dataShowTypes = $dataBase->query($requeteShowTypeSql);
 // la méthode 'fetchAll(PDO::FETCH_OBJ)' retourne un tableau d'objet des resultats de la requete sql
 $listShowTypes = $dataShowTypes->fetchAll(PDO::FETCH_OBJ);
+
+// Ex3 : Afficher les 20 premiers clients.
+// requete sql
+$requeteClientsLimitSql = 'SELECT UPPER(`lastName`) AS lastname,`firstName` FROM `clients` ORDER BY `lastName` LIMIT 20';
+// on fait appel à la méthode 'query' à qui on donne la requete sql qui nous retourne une instance d'objet PDOstatement
+$dataClientsLimit = $dataBase->query($requeteClientsLimitSql);
+// la méthode 'fetchAll(PDO::FETCH_OBJ)' retourne un tableau d'objet des resultats de la requete sql
+$listClientsLimit = $dataClientsLimit->fetchAll(PDO::FETCH_OBJ);
+
+// Ex4 : Afficher les clients qui possèdent une carte de fidélité.
+// requete sql
+$requeteClientsCardSql = 'SELECT UPPER(`lastName`) AS `lastname`,`firstName`'
+                       . 'FROM' 
+                       .      '`clients`'
+                       .      'INNER JOIN `cards` on `clients`.`cardNumber` = `cards`.`cardNumber`'
+                       .      'INNER JOIN `cardTypes` on `cards`.`cardTypesId` = `cardTypes`.`id`'
+                       .  'WHERE' 
+                       .      '`cardTypes`.`type` = "Fidélité"' 
+                       .  'ORDER BY `lastName`';
+// on fait appel à la méthode 'query' à qui on donne la requete sql qui nous retourne une instance d'objet PDOstatement
+$dataClientsCard = $dataBase->query($requeteClientsCardSql);
+// la méthode 'fetchAll(PDO::FETCH_OBJ)' retourne un tableau d'objet des resultats de la requete sql
+$listClientsCard = $dataClientsCard->fetchAll(PDO::FETCH_OBJ);
+
+// Ex5 : Afficher uniquement le nom et le prénom de tous les clients dont le nom commence par la lettre "M".
+// requete sql
+$requeteClientsMSql = 'SELECT UPPER(`lastName`) AS lastname,`firstName` FROM `clients` WHERE `lastname` LIKE "M%" ORDER BY `lastName`';
+// on fait appel à la méthode 'query' à qui on donne la requete sql qui nous retourne une instance d'objet PDOstatement
+$dataClientsM = $dataBase->query($requeteClientsMSql);
+// la méthode 'fetchAll(PDO::FETCH_OBJ)' retourne un tableau d'objet des resultats de la requete sql
+$listClientsM = $dataClientsM->fetchAll(PDO::FETCH_OBJ);
+
+// Ex6 : Afficher le titre de tous les spectacles ainsi que l'artiste, la date et l'heure.Trier les titres par ordre alphabétique. Afficher les résultat comme ceci : Spectacle par artiste, le date à heure..
+// requete sql
+$requeteShowsSql = 'SELECT `title` AS `show`, `performer` AS `by`, DATE_FORMAT(`date`,"%d:%m:%Y") AS `on`, `startTime` AS `at` FROM `shows` ORDER BY `title` ASC;';
+// on fait appel à la méthode 'query' à qui on donne la requete sql qui nous retourne une instance d'objet PDOstatement
+$dataShows = $dataBase->query($requeteShowsSql);
+// la méthode 'fetchAll(PDO::FETCH_OBJ)' retourne un tableau d'objet des resultats de la requete sql
+$listShows = $dataShows->fetchAll(PDO::FETCH_OBJ);
+
+// Ex7 : Afficher le titre de tous les spectacles ainsi que l'artiste, la date et l'heure.Trier les titres par ordre alphabétique. Afficher les résultat comme ceci : Spectacle par artiste, le date à heure..
+// requete sql
+$requeteShowsSql = 'SELECT UPPER(`lastName`) AS lastname,`firstName` FROM `clients` ORDER BY `lastName`';
+// on fait appel à la méthode 'query' à qui on donne la requete sql qui nous retourne une instance d'objet PDOstatement
+$dataShows = $dataBase->query($requeteShowsSql);
+// la méthode 'fetchAll(PDO::FETCH_OBJ)' retourne un tableau d'objet des resultats de la requete sql
+$listShows = $dataShows->fetchAll(PDO::FETCH_OBJ);
+
--- partie1/index.php
+++ partie1/index.php
@@ -7,6 +7,7 @@ include_once '../assets/php/ctrl.php';
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link rel="stylesheet" href="../assets/css/index.css" />
+        <link href="https://fonts.googleapis.com/css?family=PT+Sans&display=swap" rel="stylesheet">
         <title>pdo</title>
     </head>
     <body>
@@ -16,7 +17,8 @@ include_once '../assets/php/ctrl.php';
         <h1>Programmation orientée objet PHP</h1>
         <h2>Partie 1</h2>
         <div id="answerEx1">
-          <h3>Exercice 1 : Afficher tous les clients.</h3>
+          <h3>Exercice 1 :</h3>
+          <p>Afficher tous les clients.</p>
           <?php 
           if(isset($sqlError)){
             ?><p><?=$sqlError; ?></p><?php
@@ -32,19 +34,71 @@ include_once '../assets/php/ctrl.php';
         </div>
         <hr>
         <div id="answerEx2">
-          <h3>Exercice 2 : Afficher tous les types de spectacles possibles.</h3>
-          <?php 
-          if(isset($sqlError)){
-            ?><p><?=$sqlError; ?></p><?php
-          } else {
-            ?><select><?php
+          <h3>Exercice 2 :</h3>
+          <p>Afficher tous les types de spectacles possibles.</p>
+            <?php
             // affichage en objet des résultats de la requete client
             foreach($listShowTypes as $showTypes){
-              ?><option><?=$showTypes->type; ?></option><?php
+              ?><button><?=$showTypes->type; ?></button><?php
             }
-            ?></select><?php
-          }
-          ?>
+            ?>
+          
+        </div>
+        <hr>
+        <div id="answerEx3">
+          <h3>Exercice 3 :</h3>
+          <p>Afficher les 20 premiers clients.</p>
+          <table>
+            <?php
+            // affichage en objet des résultats de la requete client
+            foreach($listClientsLimit as $clientsLimit){
+              ?><th><?=$clientsLimit->lastname . ' ' . $clientsLimit->firstName; ?></th><?php
+            }
+            ?>
+          </table>
+        </div>
+        <hr>
+        <div id="answerEx4">
+          <h3>Exercice 4 :</h3>
+          <p>N'afficher que les clients possédant une carte de fidélité.</p>
+          <table>
+            <?php
+            // affichage en objet des résultats de la requete client
+            foreach($listClientsCard as $clients){
+              ?><th><?=$clients->lastname . ' ' . $clients->firstName; ?></th><?php
+            }
+            ?>
+          </table>
+        </div>
+        <hr>
+        <div id="answerEx5">
+          <h3>Exercice 5 :</h3>
+          <p>Afficher uniquement le nom et le prénom de tous les clients dont le nom commence par la lettre "M".</p>
+          <table>
+            <?php
+            // affichage en objet des résultats de la requete client
+            foreach($listClientsM as $clients){
+              ?><th><?= $clients->lastname . ' ' . $clients->firstName; ?></th><?php
+            }
+            ?>
+          </table>
+        </div>
+        <div id="answerEx6">
+          <h3>Exercice 6 : </h3>
+          <p>Afficher le titre de tous les spectacles ainsi que l'artiste, la date et l'heure. 
+            Trier les titres par ordre alphabétique. 
+            Afficher les résultat comme ceci : Spectacle par artiste, le date à heure.
+          </p>
+          <table>
+            <?php
+            // affichage en objet des résultats de la requete client
+            foreach($listShows as $shows){
+              ?><th><?= 'Spectable : ' . $shows->show . ' par ' . $shows->by . ', le ' . $shows->on . ' à ' . $shows->at ; ?></th><?php
+            
+              //date("d-m-Y", strtotime($shows->on))
+            }
+            ?>
+          </table>
         </div>
       </div>
     </body>
